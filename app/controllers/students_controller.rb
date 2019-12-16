6d1a150089c3c3af419c50f058dfed488b96814c
Valentine <valentinemaillard1@gmail.com>
2019-12-16 11:57:04 -0500

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,17 @@
+class StudentsController < ApplicationController
+
+    def show
+        @student = Student.find(params[:id])
+    end
+
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.find(params[:id])
+        @schooclass.create(params.require(:student).permit(:first_name, :last_name))
+        redirect_to student_path(@student)
+    end
+
+end
